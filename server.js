ac9297ebac3bab47aafac7fdfaa58821df39dd99
user <ulises.a.rivero.garcia@gmail.com>
2024-05-22 11:46:26 -0600

    added the "index" route

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
+
 /* ***********************
  * Require Statements
  *************************/
@@ -15,6 +17,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+
 
 /* ***********************
  * View Engine and Templates
@@ -38,10 +42,39 @@ folder, and the template will be named "layout".
 */
 app.set("layout", "./layouts/layout") // not at views root
 
+
+
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+//Index route
+/*
+app.get - The espress application will watch the "get" object, 
+          within the HTTP Request, for a particular route.
+
+"/" - This is route being watched. It indicates the base route 
+      of the application or the route which has no specific re- 
+      source requested.
+
+res.render() - The "res" is the response object, while "render()" 
+               is and Express function that will retrieve the spe-
+               cified view - "index" - to be sent back to the 
+               browser.
+
+{title:"Home"} - The curly braces are an object (treated like a 
+                 variable), which holds a name - value pair. This 
+                 object supplies the value that the "head" partial 
+                 file expects to receive. The object is passed to 
+                 the view. 
+
+}) - The right curly brace closes the function, while the right 
+     parentheses closes the "get" route.
+*/
+app.get("/", function(req, res) {
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
@@ -49,6 +82,8 @@ app.use(static)
  *************************/
 const port = process.env.PORT
 const host = process.env.HOST
+
+
 
 /* ***********************
  * Log statement to confirm server operation
