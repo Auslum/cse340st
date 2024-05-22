2fed5914ee59a96dac8be5edf7fcc9cd530c6cf9
user <ulises.a.rivero.garcia@gmail.com>
2024-05-22 08:26:06 -0600

    added the engine views

--- server.js
+++ server.js
@@ -6,9 +6,37 @@
  * Require Statements
  *************************/
 const express = require("express")
+/*
+We tell the application to require express-ejs-layouts, so 
+it can be used. This follows a simple rule, "You have to 
+have something in order to use it".
+*/
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+/*
+We declare that ejs will be the view engine for our application. 
+Built into EJS is the undestanding that all views will be stored 
+in a "views" folder, and that is where EJS will look for all view 
+files.
+*/
+app.set("view engine", "ejs")
+/*
+We tell the application to use the express-ejs-layouts package, 
+which has been stored into a "expressLayouts" variable. 
+*/
+app.use(expressLayouts)
+/*
+This line says that when the express ejs layout goes looking for 
+the basic template of a view, it will find it in the "layouts" 
+folder, and the template will be named "layout".
+*/
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
